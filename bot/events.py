7186c20fa3024ed79fb675752f92dcc78182efab
Nathanael Farley <nasfarley88@gmail.com>
2016-05-02 13:33:39 +0100

    Added old functions to new bot, but not event fns.

--- bot/events.py
+++ bot/events.py
@@ -1,3 +1,4 @@
+import os
 import re
 import telegram
 import datetime
--- main.py
+++ main.py
@@ -5,13 +5,14 @@ from telepot.async.delegate import per_chat_id, create_open
 
 # from . import bot as oldbot
 
-from eventbot import EventBot
+# from eventbot import EventBot
+from dotabot import DotaBot
 
 
 TOKEN = sys.argv[1]  # get token from command-line
 
 bot = telepot.async.DelegatorBot(TOKEN, [
-    (per_chat_id(), create_open(EventBot, timeout=10)),
+    (per_chat_id(), create_open(DotaBot, timeout=10)),
 ])
 
 loop = asyncio.get_event_loop()
